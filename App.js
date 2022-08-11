17df696ac1a23670c7d823fe9e240bd7f8f194d2
sasi12781a <sasi.chimmili@xcubelabs.com>
2022-08-11 12:46:21 +0530

    m

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, TextInput, View,TouchableOpacity } from "react-native";
+import {TextInput, View,TouchableOpacity } from "react-native";
 import Icon from 'react-native-vector-icons/Ionicons';
 
 function App() {
