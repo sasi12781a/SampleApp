2645b2518cc1751ee70c6f1b22aca911e457df45
sasi12781a <sasi.chimmili@xcubelabs.com>
2022-08-11 12:43:23 +0530

    m

--- App.js
+++ App.js
@@ -2,104 +2,100 @@ import React, { useState } from "react";
 import { StyleSheet, Text, TextInput, View,TouchableOpacity } from "react-native";
 import Icon from 'react-native-vector-icons/Ionicons';
 
-const App = () => {
-  const [Input,setInput]=useState('');
-  const [pass,setPass]=useState('');
-  const [secure,setSecure] =useState(true);
-  const [focus,setFocus]=useState(false);
-  const [e,setE]=useState(false);
+function App() {
+  const [Input, setInput] = useState('');
+  const [pass, setPass] = useState('');
+  const [secure, setSecure] = useState(true);
+  const [focus, setFocus] = useState(false);
+  const [e, setE] = useState(false);
 
-  const Focus=()=>{
-    setFocus(!focus)
-  }
-  
-  const Ef=()=>{
-    setE(!e)
+  function Focus() {
+    setFocus(!focus);
   }
 
-  const SecureText=()=>{
-    setSecure(!secure)
+  function Ef() {
+    setE(!e);
   }
 
-  if (Input.length==0 && pass.length==0){
-    return(
-      <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
-          <TextInput
-            style={{margin:20,color: 'black',fontSize: 17,fontWeight: 'bold',borderColor:'black',borderWidth:2,width:'80%'}}
-            value={Input}
-            onChangeText={setInput}
-            secureTextEntry={true}
-            autoCompleteType="off"
-            autoFocus={e}
-            placeholder='Email'
-            onFocus={Ef}
-          />
-          <TextInput
-            style={{color: 'black',fontSize: 17,fontWeight: 'bold',borderColor:'black',borderWidth:2,width:'80%'}}
-            value={pass}
-            onChangeText={setPass}
-            secureTextEntry={true}
-            autoCompleteType="off"
-            autoFocus={focus}
-            placeholder='Password'
-            onFocus={Focus}
-          />
+  function SecureText() {
+    setSecure(!secure);
+  }
+
+  if (Input.length == 0 && pass.length == 0) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <TextInput
+          style={{ margin: 20, color: 'black', fontSize: 17, fontWeight: 'bold', borderColor: 'black', borderWidth: 2, width: '80%' }}
+          value={Input}
+          onChangeText={setInput}
+          secureTextEntry={true}
+          autoCompleteType="off"
+          autoFocus={e}
+          placeholder='Email'
+          onFocus={Ef} />
+        <TextInput
+          style={{ color: 'black', fontSize: 17, fontWeight: 'bold', borderColor: 'black', borderWidth: 2, width: '80%' }}
+          value={pass}
+          onChangeText={setPass}
+          secureTextEntry={true}
+          autoCompleteType="off"
+          autoFocus={focus}
+          placeholder='Password'
+          onFocus={Focus} />
       </View>
 
-  
-    )
+
+    );
   }
-  return(
-    <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <View
         style={{
           marginTop: 10,
           backgroundColor: 'white',
           flexDirection: 'row',
-          width:"80%",
+          width: "80%",
           borderRadius: 6,
           borderWidth: 2,
-          borderColor:'black',
+          borderColor: 'black',
         }}>
-        <Icon style={{padding: 15}} name='mail-outline' size={30} color='#000' />
+        <Icon style={{ padding: 15 }} name='mail-outline' size={30} color='#000' />
         <TextInput
-          style={{color: 'black',fontSize: 17,fontWeight: 'bold'}}
+          style={{ color: 'black', fontSize: 17, fontWeight: 'bold' }}
           value={Input}
           onChangeText={setInput}
           autoCompleteType="off"
           autoFocus={e}
           placeholder='Email'
-          onFocus={Ef}
-        />
+          onFocus={Ef} />
       </View>
-    
+
       <View
         style={{
           marginTop: 10,
           backgroundColor: 'white',
           flexDirection: 'row',
-          width:"80%",
+          width: "80%",
           borderRadius: 6,
           borderWidth: 2,
-          borderColor:'black',
+          borderColor: 'black',
         }}>
         <TouchableOpacity onPress={SecureText}>
-          <Icon style={{padding: 15}} name={secure ? 'eye-off-outline' : "eye-outline"} size={30} color='#000' />
+          <Icon style={{ padding: 15 }} name={secure ? 'eye-off-outline' : "eye-outline"} size={30} color='#000' />
         </TouchableOpacity>
         <TextInput
-          style={{color: 'black',fontSize: 17,fontWeight: 'bold'}}
+          style={{ color: 'black', fontSize: 17, fontWeight: 'bold' }}
           value={pass}
           onChangeText={setPass}
           secureTextEntry={secure}
           placeholder='Password'
           autoCompleteType="off"
           autoFocus={focus}
-          onFocus={Focus}
-        />
+          onFocus={Focus} />
       </View>
     </View>
-  )
-  
-};
+  );
+
+}
 
 export default App;
